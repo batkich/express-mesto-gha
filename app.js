2264ce5b513acab878f1066d66d0ca1ea659fca5
Alexandr kudryavtsev <santyagobatkich@yandex.ru>
2022-04-28 12:03:58 +0700

    fix 1

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.post('/signin', celebrate({
 }), login);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().min(2).max(30),
+    email: Joi.string().email(),
     password: Joi.string().required().min(2).max(30),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
