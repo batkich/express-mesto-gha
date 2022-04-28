493f298e072c4e96b09e0fe3243d3922b8a23cc6
Alexandr kudryavtsev <santyagobatkich@yandex.ru>
2022-04-28 11:49:28 +0700

    fix 1

--- controllers/users.js
+++ controllers/users.js
@@ -124,8 +124,8 @@ const login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'topsecret-token', { expiresIn: '7d' });
-      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true })
-        .end();
+      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true });
+      res.send({ token });
     })
     .catch(() => {
       next(new Unauthorized('Авторизация неуспешна, проверьте логин или пароль'));
